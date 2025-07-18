c23a726604066dea4b166a88b607647aa74ae53e
jaav <jef@virtualsushi.be>
2025-07-18 14:43:35 +0200

    faces

--- README.md
+++ README.md
@@ -1,19 +1,171 @@
-# Hello World
+# Happy2 Application
 
-A new Flutter project created with FlutLab - https://flutlab.io
+This application displays a question "How are you doing today?" and provides emoticon options for the user to select their mood.
 
-## Getting Started
+## Font Awesome Implementation
 
-A few resources to get you started if this is your first Flutter project:
+The application is currently using Material Icons for the emoticons, but the requirement is to use Font Awesome icons instead. To implement this:
 
-- https://flutter.dev/docs/get-started/codelab
-- https://flutter.dev/docs/cookbook
+1. Make sure the `font_awesome_flutter` package is added to your `pubspec.yaml` file:
+   ```yaml
+   dependencies:
+     flutter:
+       sdk: flutter
+     flutter_localizations:
+       sdk: flutter
+     intl: ^0.20.2
+     font_awesome_flutter: ^10.6.0
+   ```
 
-For help getting started with Flutter, view our
-https://flutter.dev/docs, which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+2. Run the following command to install the package:
+   ```
+   flutter pub get
+   ```
 
-## Getting Started: FlutLab - Flutter Online IDE
+3. Update the `main.dart` file to use Font Awesome icons:
+   - Import the package: `import 'package:font_awesome_flutter/font_awesome_flutter.dart';`
+   - Replace the Material Icons with Font Awesome icons:
 
-- How to use FlutLab? Please, view our https://flutlab.io/docs
-- Join the discussion and conversation on https://flutlab.io/residents
+```dart
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// Simple translations class that can be expanded later for proper localization
+class AppTranslations {
+  static String get howAreYouTitle => 'How are you doing today?';
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Hello World',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+          Text(
+            AppTranslations.howAreYouTitle,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Very unhappy face - red
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: const FaIcon(
+                    FontAwesomeIcons.faceFrown,
+                    color: Colors.red,
+                    size: 40,
+                  ),
+                ),
+              ),
+              // Unhappy face - orange
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: const FaIcon(
+                    FontAwesomeIcons.faceFrownOpen,
+                    color: Colors.orange,
+                    size: 40,
+                  ),
+                ),
+              ),
+              // Neutral face - yellow
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: const FaIcon(
+                    FontAwesomeIcons.faceMeh,
+                    color: Colors.yellow,
+                    size: 40,
+                  ),
+                ),
+              ),
+              // Happy face - light green
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: const FaIcon(
+                    FontAwesomeIcons.faceSmile,
+                    color: Colors.lightGreen,
+                    size: 40,
+                  ),
+                ),
+              ),
+              // Very happy face - green
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: const FaIcon(
+                    FontAwesomeIcons.faceSmileBeam,
+                    color: Colors.green,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
+```
+
+## Current Implementation
+
+The current implementation uses Material Icons as a temporary solution until the Font Awesome package can be properly installed. The Material Icons used are:
+
+- Very unhappy: `Icons.sentiment_very_dissatisfied` (red)
+- Unhappy: `Icons.sentiment_dissatisfied` (orange)
+- Neutral: `Icons.sentiment_neutral` (yellow)
+- Happy: `Icons.sentiment_satisfied` (light green)
+- Very happy: `Icons.sentiment_very_satisfied` (green)
+
+To switch to Font Awesome icons, follow the steps above.
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,209 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() => runApp(MyApp());
+// Simple translations class that can be expanded later for proper localization
+class AppTranslations {
+  static String get howAreYouTitle => 'How are you doing today?';
+}
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page for Jef'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+          Text(
+            AppTranslations.howAreYouTitle,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+          // Font Awesome icons with colored backgrounds and black outlines
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Very unhappy face - red
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: Stack(
+                    children: [
+                      // Colored background
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      // Black icon for mouth/eyes
+                      const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.faceFrown,
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Unhappy face - orange
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: Stack(
+                    children: [
+                      // Colored background
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      // Black icon for mouth/eyes
+                      const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.faceFrownOpen,
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Neutral face - yellow
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: Stack(
+                    children: [
+                      // Colored background
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.yellow,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      // Black icon for mouth/eyes
+                      const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.faceMeh,
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Happy face - light green
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: Stack(
+                    children: [
+                      // Colored background
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.lightGreen,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      // Black icon for mouth/eyes
+                      const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.faceSmile,
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Very happy face - green
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(8),
+                  child: Stack(
+                    children: [
+                      // Colored background
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      // Black icon for mouth/eyes
+                      const Center(
+                        child: FaIcon(
+                          FontAwesomeIcons.faceSmileBeam,
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
